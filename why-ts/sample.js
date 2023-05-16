10f48d0bb8c6fea02fc9c1ed912593dc58f23ea3
2sangmin <rkdmf7220@gmail.com>
2023-05-16 18:02:53 +0900

    인프런 ts 강의 1일차

---
+++ why-ts/sample.js
@@ -0,0 +1,18 @@
+// function sum(a, b) {
+//   return a + b;
+// }
+// sum(10, '20')
+// let result = sum(10, '20')
+// result.toLocaleString()
+
+// @ts-check
+
+/**
+ *
+ * @param {number} a 1st number
+ * @param {number} b 2nd number
+ */
+function sum(a, b) {
+  return a + b;
+}
+sum(10, '20');
